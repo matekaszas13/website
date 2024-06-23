87d97f4b1b3544f7f0ca9ce57f572648bf75c662
matekaszas13 <matekaszas17@gmail.com>
2024-06-23 19:10:35 +0200

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ export async function getStaticProps({ locale }: any) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
-      // Will be passed to the page component as props
     },
   };
 }
